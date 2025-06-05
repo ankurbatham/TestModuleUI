d2839ecf8c3c9109ff4439e64e9ac83847d7d670
TeenaPal <pteena495@gmail.com>
2025-06-05 23:32:16 +0530

    props drilling case use in faq project

--- Basic_React_learn/faq_project/Qna/components/Faqs.jsx
+++ Basic_React_learn/faq_project/Qna/components/Faqs.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { question } from '../src/test'
 function Faqs() {
+  let[currentId , setcurrentId] = useState(question[0].id)
+let items = question.map((itemdata,i)=>{
+  let itemDetails = {
+  itemdata , 
+  currentId ,
+  setcurrentId
+  }
+ return( <FaqItem itemDetails = {itemDetails} key={i}></FaqItem>)
+})
   return (
     <div>
       <h1>Frequently asked question(FAQs)</h1>
       <div className="faqOuter">
-       <FaqInner></FaqInner>
-       <FaqInner></FaqInner>
-      <FaqInner></FaqInner>
-      <FaqInner></FaqInner>
+       {items}
       </div>
     </div>
   )
 }
-
-export function FaqInner(){
- return(
-   <div className="faqInner">
-            <h2 >hello</h2>
-            <p ></p>
+ 
+function FaqItem({itemDetails}){
+  return(
+      <div className="faqInner">
+            <h2 onClick={()=>itemDetails.setcurrentId(itemDetails.itemdata.id)}>{itemDetails.itemdata.question}</h2>
+            <p className={itemDetails.currentId===itemDetails.itemdata.id ? 'viewAns':""}>{itemDetails.itemdata.answer}</p>
           </div>
- )
+  )
 }
+
 
 export default Faqs 
 
